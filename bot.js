9920854e536e5ba28b4f7faff2117a1657131647
dragonboyytd <42145713+dragonboyytd@users.noreply.github.com>
2018-09-18 18:29:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,41 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+
+const adminprefix = "/";
+const devs = ['344487452200796160'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setg')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setn')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'seta')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'sett')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
+
+
+
+
+
+
+
+
+
 client.on('ready', () => {
   console.log('---------------');
   console.log('Dragon Bot')
